69ed50ef7f2ea1c9836d74079c9487d0196b53d3
MarcinPepke <102230@stud.uz.zgora.pl>
2022-06-22 19:23:45 +0200

    Added generating token method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
+import com.neovisionaries.i18n.CountryCode;
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.requests.data.browse.GetListOfNewReleasesRequest;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException, SpotifyWebApiException {
+
+
         SpotifyAlbum spotifyAlbum = new SpotifyAlbum();
         System.out.println(spotifyAlbum.getArtist());
+        System.out.println(spotifyAlbum.getAlbumName());
         System.out.println(spotifyAlbum.getAlbumType());
-        System.out.println(spotifyAlbum.getImgAdress());
+        System.out.println(spotifyAlbum.getImgAddress());
         System.out.println(spotifyAlbum.getSpotifyHref());
+
+
+
     }
 }
--- src/main/java/SpotifyAlbum.java
+++ src/main/java/SpotifyAlbum.java
@@ -2,80 +2,115 @@ import com.neovisionaries.i18n.CountryCode;
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
 import se.michaelthelin.spotify.model_objects.specification.*;
+import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import se.michaelthelin.spotify.requests.data.browse.GetListOfNewReleasesRequest;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpotifyAlbum {
-    final static String BEARER = "BQCtJTKhLFuCzu-GScRAqy1yQl4W4IPAOZdakl9fxywCYcL5Wj0XCwgIdn1YfBDKZF-d5rjB7rruKgcNgKr6XivKytppEuV41QJulesG0ow1vwUu7QVGcaTydmtMCs9LmsoehcDZ8ahtXlUJ06kNWOQ3vqp5zSgKAe_8QDRB13Ky9P9QvRH6gaW78pAvLg7vEkLHG_xEyQZ3aCWWzWk";
+    static final String clientId = "2bcb05b4c8094c44958c34ecb9391751";
+    static final String clientSecret = "273ab3cb8a6a46b9a027a539a260b237";
 
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId("2bcb05b4c8094c44958c34ecb9391751")
-            .setClientSecret("273ab3cb8a6a46b9a027a539a260b237")
-            .setAccessToken(BEARER)
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
             .build();
 
 
-    private static final GetListOfNewReleasesRequest getListOfNewReleasesRequest = spotifyApi.getListOfNewReleases()
-            .country(CountryCode.PL)
-            .limit(5)
-            .offset(1)
-            .build();
+    public String getBearer() throws IOException, ParseException, SpotifyWebApiException {
+        ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+        System.out.println(clientCredentials.getAccessToken());
+        return clientCredentials.getAccessToken();
+    }
+
+
+    public GetListOfNewReleasesRequest releasesRequestPL() throws IOException, ParseException, SpotifyWebApiException {
+        spotifyApi.setAccessToken(getBearer());
+        return spotifyApi.getListOfNewReleases()
+                .country(CountryCode.PL)
+                .limit(5)
+                .offset(0)
+                .build();
+    }
 
     public List<String> getArtist() throws IOException, ParseException, SpotifyWebApiException {
-        final Paging<AlbumSimplified> album = getListOfNewReleasesRequest.execute();
+        final Paging<AlbumSimplified> album = releasesRequestPL().execute();
+        StringBuilder artists = new StringBuilder();
         List<String> artistsNames = new ArrayList<>();
-        for (AlbumSimplified items : album.getItems()
+        for (AlbumSimplified albumItems : album.getItems()
         ) {
-            for (ArtistSimplified artist : items.getArtists()
+            for (ArtistSimplified artist : albumItems.getArtists()
             ) {
-                if (items.getReleaseDate().equals(LocalDate.now().toString()))
+                if (albumItems.getArtists().length > 1)
+                    artists.append(artist.getName()).append(", ");
+                else
                     artistsNames.add(artist.getName());
-            }
 
+            }
+            if (artists.toString().length() > 0) {
+                artists.delete(artists.toString().length() - 2, artists.toString().length());
+                artistsNames.add(artists.toString());
+                artists = new StringBuilder();
+            }
         }
+
         return artistsNames;
     }
 
     public List<String> getAlbumType() throws IOException, ParseException, SpotifyWebApiException {
-        final Paging<AlbumSimplified> album = getListOfNewReleasesRequest.execute();
+        final Paging<AlbumSimplified> album = releasesRequestPL().execute();
         List<String> albumTypesList = new ArrayList<>();
         for (AlbumSimplified albumItems :
                 album.getItems()) {
-            if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
-                albumTypesList.add(albumItems.getAlbumType().getType());
+//            if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
+            albumTypesList.add(albumItems.getAlbumType().getType());
         }
         return albumTypesList;
     }
 
-    public String getImgAdress() throws IOException, ParseException, SpotifyWebApiException {
-        final Paging<AlbumSimplified> album = getListOfNewReleasesRequest.execute();
+    public String getImgAddress() throws IOException, ParseException, SpotifyWebApiException {
+        final Paging<AlbumSimplified> album = releasesRequestPL().execute();
         for (AlbumSimplified albumItems :
                 album.getItems()) {
             for (Image img :
                     albumItems.getImages()
             ) {
-                if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
-                    return img.getUrl();
+//                if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
+                return img.getUrl();
             }
         }
         return null;
     }
 
     public List<String> getSpotifyHref() throws IOException, ParseException, SpotifyWebApiException {
-        final Paging<AlbumSimplified> album = getListOfNewReleasesRequest.execute();
+        final Paging<AlbumSimplified> album = releasesRequestPL().execute();
         List<String> spotifyAlbumUrls = new ArrayList<>();
         for (AlbumSimplified albumItems :
                 album.getItems()) {
-            if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
-                albumItems.getExternalUrls().getExternalUrls().forEach((s, s2) -> spotifyAlbumUrls.add(s2));
+//            if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
+            albumItems.getExternalUrls().getExternalUrls().forEach((s, s2) -> spotifyAlbumUrls.add(s2));
 
         }
         return spotifyAlbumUrls;
+    }
+
+    public List<String> getAlbumName() throws IOException, ParseException, SpotifyWebApiException {
+        final Paging<AlbumSimplified> albumSimplifiedPaging = releasesRequestPL().execute();
+        List<String> spotifyAlbumNames = new ArrayList<>();
+        for (AlbumSimplified albumItems :
+                albumSimplifiedPaging.getItems()) {
+//            if (albumItems.getReleaseDate().equals(LocalDate.now().toString()))
+            spotifyAlbumNames.add(albumItems.getName());
+
+        }
+        return spotifyAlbumNames;
     }
 
 
